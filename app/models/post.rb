b3a64de312cb16e44c72fc4b3cd49890ce72c32c
ernesto <ernesto002@icloud.com>
2021-03-29 16:41:38 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true 
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    BAIT = [/Won't Believe/i, /Secrete/i, /Top [0-9]*/i, /Guess/i]
+    validate :bait?
+
+    def bait?
+        if !BAIT.find{|b| b.match title}
+            errors.add(:title, "add click-bait to title")
+        end
+    end
 end
